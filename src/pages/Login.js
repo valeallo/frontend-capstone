fd212284eb5308412eeed2ab27ba594a2f4f20ae
valeallo <82455132+valeallo@users.noreply.github.com>
2023-12-17 13:47:57 +0100

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const res = await axios.post('http://localhost:5000/users/login', formState)
+      const res = await axios.post('${process.env.REACT_APP_API_URL}/users/login', formState)
       if (res.status === 200) {
         sessionStorage.setItem('authorization', JSON.stringify(res.data))
         navigate('../dashboard', { replace: true })
